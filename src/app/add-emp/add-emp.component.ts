982beec16b6d1d006ca6478ba24abbd77da939a4
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-07 01:50:36 +0530

    api commit

--- src/app/add-emp/add-emp.component.ts
+++ src/app/add-emp/add-emp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-emp',
@@ -17,11 +18,19 @@ export class AddEmpComponent {
   username=""
   password=""
 
-
+  constructor(private api:ApiService) {}
   readValue=()=>
   {
     let data:any={"empcode":this.empcode,"empname":this.empname,"designation":this.designation,"salary":this.salary,"companyname":this.companyname,"mobile":this.mobile,"username":this.username,"password":this.password}
     console.log(data)
+    this.api.addEmployee(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        
+      }
+    )
+  }
 
 }
-}
+
